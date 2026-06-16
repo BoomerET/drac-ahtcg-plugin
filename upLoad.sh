7ab9e117b079f12ec52dc4c78147bee0729c1d32
David Berkompas <david.berkompas@gmail.com>
2026-06-15 19:16:19 -0500

    Changing things to see if actions is working

--- upLoad.sh
+++ upLoad.sh
@@ -1,1 +1,1 @@
-tar -czf jsons.tar.gz jsons && curl -X POST http:/192.168.7.254:4000/api/plugin-repo-update -F "repo_url=https://github.com/BoomerET/drac-ahtcg-plugin" -F "file=@jsons.tar.gz"
+tar -czf jsons.tar.gz jsons && curl -X POST http:/192.168.7.142:4000/api/plugin-repo-update -F "repo_url=https://github.com/BoomerET/drac-ahtcg-plugin" -F "file=@jsons.tar.gz"
